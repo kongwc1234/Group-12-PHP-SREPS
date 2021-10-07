a446cc9f8db6a9519d4852cc2e3d1e33bf74636e
kongwc1234 <59855225+kongwc1234@users.noreply.github.com>
2021-10-07 18:13:03 +0800

    Add files via upload

---
+++ php_sreps.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 07, 2021 at 12:12 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.3.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_sreps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `staff_id` char(5) NOT NULL,
+  `staff_username` varchar(30) NOT NULL,
+  `staff_password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` char(5) NOT NULL,
+  `cat_name` varchar(50) NOT NULL,
+  `cat_description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` char(5) NOT NULL,
+  `item_name` varchar(50) NOT NULL,
+  `item_description` varchar(255) NOT NULL,
+  `item_price` decimal(4,2) NOT NULL,
+  `item_stock` int(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_category`
+--
+
+CREATE TABLE `item_category` (
+  `item_id` char(5) NOT NULL,
+  `cat_id` char(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `sales_id` char(5) NOT NULL,
+  `staff_id` char(5) NOT NULL,
+  `sales_date` date NOT NULL,
+  `sales_description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_item`
+--
+
+CREATE TABLE `sales_item` (
+  `sales_id` char(5) NOT NULL,
+  `item_id` char(5) NOT NULL,
+  `quantity` int(3) UNSIGNED NOT NULL,
+  `quantity_price` decimal(4,2) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `staff_id` char(5) NOT NULL,
+  `staff_fname` varchar(30) NOT NULL,
+  `staff_lname` varchar(30) NOT NULL,
+  `staff_dob` date NOT NULL,
+  `staff_address` varchar(255) NOT NULL,
+  `staff_phone` varchar(15) NOT NULL,
+  `staff_gender` char(1) DEFAULT 'U'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`staff_id`),
+  ADD UNIQUE KEY `staff_username` (`staff_username`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `item_category`
+--
+ALTER TABLE `item_category`
+  ADD PRIMARY KEY (`item_id`,`cat_id`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`sales_id`),
+  ADD KEY `staff_id` (`staff_id`);
+
+--
+-- Indexes for table `sales_item`
+--
+ALTER TABLE `sales_item`
+  ADD PRIMARY KEY (`sales_id`,`item_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`staff_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`);
+
+--
+-- Constraints for table `item_category`
+--
+ALTER TABLE `item_category`
+  ADD CONSTRAINT `item_category_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`),
+  ADD CONSTRAINT `item_category_ibfk_2` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`);
+
+--
+-- Constraints for table `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`);
+
+--
+-- Constraints for table `sales_item`
+--
+ALTER TABLE `sales_item`
+  ADD CONSTRAINT `sales_item_ibfk_1` FOREIGN KEY (`sales_id`) REFERENCES `sales` (`sales_id`),
+  ADD CONSTRAINT `sales_item_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
