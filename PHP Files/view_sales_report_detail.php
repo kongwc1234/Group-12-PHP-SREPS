c9539b770cc0022de9c52ce9b3338950be3e9a1e
kongwc1234 <59855225+kongwc1234@users.noreply.github.com>
2021-11-04 22:14:28 +0800

    Add files via upload

---
+++ PHP Files/view_sales_report_detail.php
@@ -0,0 +1,163 @@
+<?php
+	session_start();
+	require_once('mysqli_connect.php');
+	$sql = "SELECT * FROM sales_item WHERE sales_id = '".$_POST['key']."'";
+    $result = mysqli_query($dbc, $sql);
+	$row = mysqli_fetch_assoc($result);
+	$sql = "SELECT * FROM sales NATURAL JOIN staff WHERE sales_id = '".$_POST['key']."'";
+	$result = mysqli_query($dbc, $sql);
+	$row2 = mysqli_fetch_assoc($result);
+	$sql = "SELECT * FROM sales_item NATURAL JOIN item WHERE sales_id = '".$_POST['key']."'";
+    $result = mysqli_query($dbc, $sql);
+	$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	if (empty($_SESSION) || empty($_SESSION['login_user'])) 
+	{
+		header("Location: login_page.php");
+	}
+?>
+
+<html>
+	<head>
+		<title>
+			View Sales Report
+		</title>
+		<style>
+			input {
+    			border: 1.5px solid #030337;
+    			border-radius: 4px;
+    			padding: 7px 30px;
+			}
+			input[type=submit] {
+				background-color: #030337;
+				color: white;
+    			border-radius: 4px;
+    			padding: 7px 45px;
+    			margin: 0px 200px
+			}
+		</style>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
+	</head>
+	<body>
+		<img class="logo" src="logo.png"/>
+		<h1 id="title">
+			People Health Pharmacy
+		</h1>
+		<div>
+			<ul style="background-color:darkblue">
+				<li><a href="staff_homepage.php"><i class="fa fa-desktop" aria-hidden="true"></i> Dashboard</a></li>
+				<li><a href="logout_handler.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
+			</ul>
+		</div>
+			<h2>View Sales Report</h2>
+
+			<?php
+				if(isset($_GET['msg']) && $_GET['msg']=='success_delete')
+				{
+					echo "<strong style='color: green'>The sales record has been successfully deleted.</strong>
+						<br>
+						<br>";
+				}
+				else if(isset($_GET['msg']) && $_GET['msg']=='failed_delete')
+				{
+					echo "<strong style='color: red'>*Invalid sales record Details, please enter again.</strong>
+						<br>
+						<br>";
+				}
+
+				if(isset($_GET['msg']) && $_GET['msg']=='success_edit')
+				{
+					echo "<strong style='color: green'>The sales record has been successfully edited.</strong>
+						<br>
+						<br>";
+				}
+				else if(isset($_GET['msg']) && $_GET['msg']=='failed_edit')
+				{
+					echo "<strong style='color: red'>*Invalid sales record Details, please enter again.</strong>
+						<br>
+						<br>";
+				}
+			?>
+
+			<?php
+			echo '<table cellpadding="5">
+				<tr>
+					<td class="fix_table">Sales ID</td>
+				</tr>
+				<tr>
+					<td class="fix_table"><input type="text" readonly="readonly" name="sales_id" value="'.$row2["sales_id"].'" maxlength="5" required></td>
+				</tr>
+			</table>
+			<br>
+			<hr>
+			
+			<table cellpadding="5">
+				<tr>
+					<td class="fix_table">Added by:</td>
+				</tr>
+				<tr>
+					<td class="fix_table"><input type="text" readonly="readonly" name="staff_id" value="'.$row2["staff_fname"].' '.$row2["staff_lname"].' ('.$row2["staff_id"].')" maxlength="5" required>
+					</td>
+				</tr>
+			</table>
+			<br>
+			<hr>
+			
+			<table cellpadding="5">
+			<tr>
+					<td class="fix_table">Sales Date</td>
+				</tr>
+				<tr>
+					<td class="fix_table"><input type="text" readonly="readonly" name="sales_date" value="'.$row2["sales_date"].'" maxlength="5" required></td>
+				</tr>
+			</table>
+			<br>
+			<hr>
+			
+			<table cellpadding="5">
+			<tr>
+					<td class="fix_table">Sales Description</td>
+				</tr>
+				<tr>
+					<td class="fix_table"><input type="text" readonly="readonly" name="sales_description" value="'.$row2["sales_description"].'" maxlength="5" required></td>
+				</tr>
+			</table>
+			<br>
+			<hr>
+			
+			<td class = "fix_table"><h2>Item Quantity &emsp; &ensp; Item Quantity Price</h2>';
+			
+			foreach($rows as $row3){
+			echo '<table cellpadding="5">
+				<tr>
+					<td class="fix_table"><b>'.$row3["item_name"].' (ID '.$row3["item_id"].')</b></td>
+				</tr>
+				<tr>
+					<td class="fix_table"><input type="number" readonly="readonly" pattern="[0-9]*" min="0" max="'.$row3["item_stock"].'" style="width:78%" name="quantity['.$row3["item_id"].']" value="'.$row3["quantity"].'"></td>
+							      <input type="hidden" name="itemid[]" value="'.$row3["item_id"].'">
+								  <td class="fix_table">'.$row3["quantity_price"].'</td>
+				</tr>
+			</table>
+			<br>
+					<td class = "fix_table">  &nbsp RM'.$row3["item_price"].' Each </td>
+			<br>
+			<br>
+			<hr>';
+			}
+			$sql = "SELECT SUM(quantity_price) AS total_price FROM sales_item WHERE sales_id = '".$_POST['key']."'";
+			$result = mysqli_query($dbc, $sql);
+			$total_price = mysqli_fetch_assoc($result);
+			echo '<table cellpadding="5">
+				<tr>
+					<td class="fix_table"><b>Total Price</b></td>
+					<td class="fix_table">'.$total_price["total_price"].'</td>
+				</tr>
+			</table>';
+		?>
+		<br>
+		<br>
+		<form method='post' action='view_sales_report.php'>
+      <input type='submit' class='w3-button w3-khaki' name='view' value='Back' style="width:10%;margin-left:auto">
+	</body>
+</html>
